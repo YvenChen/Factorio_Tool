0d96194d26d75677042af3c276f3d5c50938bce8
YvenChen <yvenchen@icloud.com>
2023-01-04 19:37:09 +0800

    Initial Commit

---
+++ factorioTool/factorioToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  factorioToolApp.swift
+//  factorioTool
+//
+//  Created by Yven Chen on 2023/1/4.
+//
+
+import SwiftUI
+
+@main
+struct factorioToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
